d10b4b4caa85813271e35e795e1b981fe79fb253
prashanth <prashanth.manravadi@gmail.com>
2017-11-10 19:32:09 -0500

    login with orco

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
-import { Redirect, Route } from 'react-router-dom';
+import { login } from '../service/client';
 
 class LoginComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            username: '',
+            password: '',
             loginSuccess: false
+        };
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.callLoginService = this.callLoginService.bind(this);
+    }
+
+    handleUsernameChange(event) {
+        this.setState({username: event.target.value});
+    }
+
+    handlePasswordChange(event) {
+        this.setState({password: event.target.value});
+    }
+
+    async callLoginService() {
+        let response = await login(this.state.username, this.state.password);
+        if(response) {
+            this.props.history.push('/dashboard');
         }
     }
 
@@ -14,25 +34,13 @@ class LoginComponent extends Component {
             <div className="wrapper">
                 <form className="form-signin">
                     <h2 className="form-signin-heading">Please login</h2>
-                    <input type="text" className="form-control" name="username" placeholder="Email Address" />
-                    <input type="password" className="form-control" name="password" placeholder="Password"/>
-                    <LoginButton/>
+                    <input type="text" className="form-control" name="username" placeholder="username" value={this.state.username} onChange={this.handleUsernameChange}/>
+                    <input type="password" className="form-control" name="password" placeholder="password" value={this.state.password} onChange={this.handlePasswordChange}/>
+                    <button type="button" className="btn btn-lg btn-primary btn-block" onClick={this.callLoginService}>Login</button>
                 </form>
             </div>
         );
     }
 }
-
-const Authenticate = ({ title, history }) => (
-    <button
-        className="btn btn-lg btn-primary btn-block"
-        onClick={() => history.push('/dashboard')}
-    >{title}
-    </button>
-);
-
-const LoginButton = () => (
-    <Route path="/" render={(props) => <Authenticate {...props} title="Login" />} />
-);
 
 export default LoginComponent;
